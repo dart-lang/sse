feb0a335cb7dc7620fc0b89fbc290a5fe8a72cf0
Gary Roumanis <groumanis@gmail.com>
2021-02-05 18:38:59 +0000

    Update documentation (#37)
    * Update documentation
    
    * more documentation

--- lib/src/server/sse_handler.dart
+++ lib/src/server/sse_handler.dart
@@ -193,6 +193,16 @@ class SseHandler {
 
   StreamQueue<SseConnection> _connectionsStream;
 
+  /// [_uri] is the URL under which the server is listening for
+  /// incoming bi-directional SSE connections.
+  ///
+  /// If [keepAlive] is supplied, connections will remain active for this
+  /// period after a disconnect and can be reconnected transparently. If there
+  /// is no reconnect within that period, the connection will be closed
+  /// normally.
+  ///
+  /// If [keepAlive] is not supplied, connections will be closed immediately
+  /// after a disconnect.
   SseHandler(this._uri, {Duration keepAlive}) : _keepAlive = keepAlive;
 
   StreamQueue<SseConnection> get connections =>
